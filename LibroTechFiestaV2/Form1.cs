7a233fe63141c7b46b2bf03678f0b1f77291adbe
Recea Alex <recea.alex@yahoo.com>
2024-04-03 22:30:35 +0300

    am creeat o conexiune test pentru baza de date si am afisat tot ce este in tabela books, urmeaza implementarea cu login, verficare de caractere

--- LibroTechFiestaV2/Form1.cs
+++ LibroTechFiestaV2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,37 @@ namespace LibroTechFiestaV2
 {
     public partial class Form1 : Form
     {
+        SqlConnection myCon = new SqlConnection();
+        DataSet dsClients;
+        DataSet dsBooks;
         public Form1()
         {
             InitializeComponent();
+            myCon.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Project_II\LibroTechFiestaV2\Database1.mdf;Integrated Security=True";
+            myCon.Open();
+            dsClients = new DataSet();
+            dsBooks = new DataSet();
+            SqlDataAdapter daClients = new SqlDataAdapter("SELECT * FROM Clients", myCon);
+            SqlDataAdapter daBooks = new SqlDataAdapter("SELECT * FROM Books", myCon);
+            daBooks.Fill(dsBooks, "Books");
+            daClients.Fill(dsClients, "Clients");
+
+            foreach (DataRow dr in dsBooks.Tables["Books"].Rows)
+            {
+                String name = dr.ItemArray.GetValue(1).ToString();
+                listBox1.Items.Add(name);
+            }
+            myCon.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
